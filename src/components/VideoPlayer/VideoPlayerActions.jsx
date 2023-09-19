6943f29f54ecf69548b3efab316c4908d0269aca
LatinGeek <germanlamelar@hotmail.com>
2023-09-19 10:31:23 -0300

    Fix action icons element

--- src/components/VideoPlayer/VideoPlayerActions.jsx
+++ src/components/VideoPlayer/VideoPlayerActions.jsx
@@ -4,20 +4,23 @@ import { Share } from "../Icons/Share";
 import { More } from "../Icons/More";
 import styles from './styles.module.css';
 
-export default function VideoPlayerActions() {
+export default function VideoPlayerActions({ likes = 124, comments = 25, shares = 6, hearted = false }) {
     return (
         <aside className={styles.actions}>
             <div className={styles.action}>
-                <Heart fillColor="#2154a6"/>
+                <Heart fillColor={hearted ? "#FF0000" : "#FFFFFF"} />
+                <strong title="likes">{likes}</strong>
             </div>
             <div className={styles.action}>
-                <Comments fillColor="#2154a6"/>
+                <Comments fillColor="#FFFFFF" />
+                <strong title="comments">{comments}</strong>
             </div>
             <div className={styles.action}>
-                <Share fillColor="#2154a6"/>
+                <Share fillColor="#FFFFFF" />
+                <strong title="shares">{shares}</strong>
             </div>
             <div className={styles.action}>
-                <More fillColor="#2154a6"/>
+                <More fillColor="#FFFFFF" />
             </div>
         </aside>
     )
